3373b450454d211759089f5dcd189e125e8f649d
Andrew <aragacalledpappas@gmail.com>
2015-06-10 15:51:42 -0400

    first two tests!

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,4 +2,10 @@ import candix_api
 
 #highest level modules exist
 def test_modules_exist():
-    assert(1 == 1)
+    assert('main' in dir(candix_api))
+    assert('logic' in dir(candix_api))
+    assert('data_access' in dir(candix_api))
+
+def test_da_states_count():
+    states = candix_api.data_access.get_states()
+    assert(len(states) == 50)
