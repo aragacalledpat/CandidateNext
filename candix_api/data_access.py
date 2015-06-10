b6a0c8374a3f4df7c08615e2754a381c308ed024
Andrew <aragacalledpappas@gmail.com>
2015-06-10 15:40:21 -0400

    change states call

--- candix_api/data_access.py
+++ candix_api/data_access.py
@@ -56,5 +56,14 @@ def get_bill_from_id(bill_id):
         bill_details.append(detail)
     return bill_details
 
+def get_states():
+    return ["AL", "AK", "AZ", "AR", "CA", "CO", "CT",
+            "DE", "FL", "GA", "HI", "ID", "IL", "IN",
+            "IA", "KS", "KY", "LA", "ME", "MD", "MA",
+            "MI", "MN", "MS", "MO", "MT", "NE", "NV",
+            "NH", "NJ", "NM", "NY", "NC", "ND", "OH",
+            "OK", "OR", "PA", "RI", "SC", "SD", "TN",
+            "TX", "UT", "VT", "VA", "WA", "WV", "WI",
+            "WY"]
 
 
--- candix_api/logic.py
+++ candix_api/logic.py
@@ -14,13 +14,5 @@ def get_bills():
             bills[bill_id][detail[0]] = detail[1]
     return bills
 
-
 def get_states():
-    return ["AL", "AK", "AZ", "AR", "CA", "CO", "CT",
-            "DE", "FL", "GA", "HI", "ID", "IL", "IN",
-            "IA", "KS", "KY", "LA", "ME", "MD", "MA",
-            "MI", "MN", "MS", "MO", "MT", "NE", "NV",
-            "NH", "NJ", "NM", "NY", "NC", "ND", "OH",
-            "OK", "OR", "PA", "RI", "SC", "SD", "TN",
-            "TX", "UT", "VT", "VA", "WA", "WV", "WI",
-            "WY"]
+    return data_access.get_states()
