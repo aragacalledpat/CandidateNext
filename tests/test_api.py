5f2417006b43f92dff93e0e25b3c583b48c2e7c8
Andrew <aragacalledpappas@gmail.com>
2015-06-17 14:31:53 -0400

    stub out some mocks

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,21 @@
 import candix_api
+import mock
+
+#mocks for data access layer
+def mock_get_candidates():
+    return ()
+
+def mock_get_candidate(candix_congress_id):
+    return ()
+
+def mock_get_bills():
+    return ()
+
+def mock_get_bill(bill_id):
+    return ()
+
+def mock_get_districts():
+    return ()
 
 #highest level modules exist
 def test_modules_exist():
@@ -6,6 +23,6 @@ def test_modules_exist():
     assert('logic' in dir(candix_api))
     assert('data_access' in dir(candix_api))
 
-def test_da_states_count():
+def test_states_count():
     states = candix_api.data_access.get_states()
     assert(len(states) == 50)
